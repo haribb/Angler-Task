85b9fad0e4182d02642ae9bed46925d244071f78
Shaik Rahuman <103171089+shaikdev@users.noreply.github.com>
2023-10-25 14:16:08 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Suspense } from "react";
+import "./App.scss";
+import Test from "./screens/test/test.screen";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./common_components/hoc/main.hoc";
+import store from "./store/store";
+import { Provider } from "react-redux";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Suspense fallback={<div />}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Main>
+                  <Test />
+                </Main>
+              }
+            ></Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+export default App;
---
+++ src/utils/imports.utils.ts
@@ -0,0 +1,12 @@
+import * as Functions from './functions.utils'
+import InputComponent from 'common_components/ui/input/input.common_components'
+import * as Validations from 'utils/validation.utils'
+
+
+
+
+export {
+    Functions,
+    InputComponent,
+    Validations
+}
